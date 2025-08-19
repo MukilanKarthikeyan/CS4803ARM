db0fcd1e331910fa29ee65246ee1ea77912c05a4
Soobum Kim <skim743@gatech.edu>
2025-08-19 14:58:51 -0400

    Added default scene with a piper arm and an optical breadboard. Directories are re-organized.

---
+++ docker/docker_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run -it \
+           --rm \
+           --net=host \
+           --name piper_sim_mujoco \
+           -e DISPLAY=$DISPLAY \
+	       -v /tmp/.X11-unix/:/tmp/.X11-unix/:rw \
+           -v "$(pwd)/../user_data:/user_data" \
+           skim743/piper_sim_mujoco
