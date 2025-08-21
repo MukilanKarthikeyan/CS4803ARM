caf31df0ddb589e9b42fd5a7d61352f80a061b7e
Soobum Kim <skim743@gatech.edu>
2025-08-21 10:15:19 -0400

    Updated the volume link path for user_data for non-gpu docker run.

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -6,5 +6,5 @@ docker run -it \
            --name piper_sim_mujoco \
            -e DISPLAY=$DISPLAY \
 	       -v /tmp/.X11-unix/:/tmp/.X11-unix/:rw \
-           -v "$(pwd)/../user_data:/user_data" \
+           -v "$(pwd)/../user_data:/CS4803_ARM/user_data" \
            skim743/piper_sim_mujoco:latest
