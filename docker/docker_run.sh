04233b63fe9d3e5a011d3422b15312e65f94e4c4
Soobum Kim <skim743@gatech.edu>
2025-08-24 19:58:11 -0400

    Reformatted docker run scripts

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -7,6 +7,6 @@ docker run -it \
            --net=host \
            --name piper_sim_mujoco \
            -e DISPLAY=$DISPLAY \
-	       -v /tmp/.X11-unix/:/tmp/.X11-unix/:rw \
+           -v /tmp/.X11-unix/:/tmp/.X11-unix/:rw \
            -v "$(pwd)/../user_data:/CS4803ARM_Fall2025/user_data" \
            skim743/piper_sim_mujoco:latest
--- docker/docker_run_gpu.sh
+++ docker/docker_run_gpu.sh
@@ -11,6 +11,6 @@ docker run -it \
            --name piper_sim_mujoco \
            -e NVIDIA_DRIVER_CAPABILITIES=all \
            -e DISPLAY=$DISPLAY \
-	       -v /tmp/.X11-unix/:/tmp/.X11-unix/:rw \
+           -v /tmp/.X11-unix/:/tmp/.X11-unix/:rw \
            -v "$(pwd)/../user_data:/CS4803ARM_Fall2025/user_data" \
            skim743/piper_sim_mujoco:latest
